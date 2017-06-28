73795acd03b5d483a09ec727914635bf8b91be22
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-28 17:37:38 +0200

    feat: edit GvrPointerInputModule Script

--- Assets/GoogleVR/Scripts/EventSystem/GvrPointerInputModule.cs
+++ Assets/GoogleVR/Scripts/EventSystem/GvrPointerInputModule.cs
@@ -141,7 +141,7 @@ public class GvrPointerInputModule : BaseInputModule {
     UpdateReticle(previousObject);
 
     // True during the frame that the trigger has been pressed.
-    bool triggerDown = Input.GetMouseButtonDown(0);
+    bool triggerDown = Input.GetMouseButtonDown(0) || input.GetButtonDown("Interact");
     // True if the trigger is held down.
     bool triggering = Input.GetMouseButton(0);
 
