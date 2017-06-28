515f9a610ea098a785528de4cf54881b70b18007
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-28 18:19:06 +0200

    feat: add poster behaviour.

---
+++ Assets/Scripts/Level01/PosterBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class PosterBehaviour : MonoBehaviour
+    {
+        public Transform dropTransform;
+
+        private bool _dropped = false;
+
+        public void OnPosterClick()
+        {
+            transform.rotation = dropTransform.rotation;
+            transform.position = dropTransform.position;
+            GetComponent<MeshCollider>().enabled = false;
+        }
+
+    }
+}
