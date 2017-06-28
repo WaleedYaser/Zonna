f8004c580cc9e7562315ed5f93d7b0991c75d8a6
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-29 00:56:17 +0200

    feat: add unblock me puzzle.

---
+++ Assets/Scripts/Level01/BlockBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class BlockBehaviour : MonoBehaviour
+    {
+
+        private Rigidbody _blockRigidBody;
+        private bool _flag = true;
+
+        // Use this for initialization
+        void Start()
+        {
+            _blockRigidBody = GetComponent<Rigidbody>();
+        }
+
+
+        public void OnBuzzleClick()
+        {
+            if (_flag)
+                _blockRigidBody.velocity = new Vector3(1, 1, 1);
+            else
+                _blockRigidBody.velocity = new Vector3(-1, -1, -1);
+            _flag = !_flag;
+        }
+    }
+}
---
+++ Assets/UnBlockMePuzzle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class UnBlockMePuzzle : MonoBehaviour
+    {
+        public Rigidbody[] blocks_rigidbodies;
+        public Transform explotion_position;
+        public GameObject puzzle_Japan;
+
+        // Use this for initialization
+        void Start()
+        {
+            puzzle_Japan.SetActive(false);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "Win_Piece")
+            {
+                foreach (var block in blocks_rigidbodies)
+                {
+                    block.useGravity = true;
+                    block.constraints = RigidbodyConstraints.None;
+                    block.AddExplosionForce(500f, explotion_position.position, 5f, 3f);
+                }
+
+                puzzle_Japan.SetActive(true);
+            }
+        }
+    }
+}
