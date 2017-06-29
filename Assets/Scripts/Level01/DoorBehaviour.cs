79f696c39c102b99bd78befdda253fdecf961246
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-29 16:45:44 +0200

    feat: add door behaviour.

---
+++ Assets/Scripts/Level01/DoorBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class DoorBehaviour : MonoBehaviour
+    {
+        public string sceneName;
+
+        public void OnDoorClick()
+        {
+            transform.Rotate(new Vector3(0, 30, 0));
+            GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(_LoadSceneAfter());
+        }
+
+        IEnumerator _LoadSceneAfter()
+        {
+            yield return new WaitForSeconds(2f);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+}
