964cdf716301bbd2b6309a5b535c0affbdc9cf94
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-29 08:30:47 +0200

    feat: add printer behaviour.

---
+++ Assets/Scripts/Level01/PrinterBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class PrinterBehaviour : MonoBehaviour
+    {
+
+        public GameObject paper;
+        private GvrAudioSource _printer_Audio;
+
+        // Use this for initialization
+        void Start()
+        {
+            _printer_Audio = GetComponent<GvrAudioSource>();
+            paper.SetActive(false);
+        }
+
+        public void OnPrinterClick()
+        {
+            _printer_Audio.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(_DoAfterPrint());
+        }
+
+        IEnumerator _DoAfterPrint()
+        {
+            yield return new WaitUntil(() => !_printer_Audio.isPlaying);
+            paper.SetActive(true);
+        }
+    }
+}
