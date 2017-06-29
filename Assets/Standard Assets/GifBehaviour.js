7fccba4e9a352f925f7cd8af24bb3be225927033
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-29 05:10:13 +0200

    fix: update GifBehaviour script.

--- Assets/Standard Assets/GifBehaviour.js
+++ Assets/Standard Assets/GifBehaviour.js
@@ -1,7 +1,10 @@
 ﻿var frames : Texture[];
  var framesPerSecond = 10;
+ var t = 0f;
+
   function Update() 
   {
-   var index : int = (Time.time * framesPerSecond) % frames.Length;
+  t = t + Time.deltaTime;
+   var index : int = (t * framesPerSecond) % frames.Length;
    GetComponent.<Renderer>().material.mainTexture = frames[index];
     }
