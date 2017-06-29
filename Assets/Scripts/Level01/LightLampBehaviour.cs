9b3d389503063391d0a11864216788ef1c480724
Waleed Yaser <waleedyaser95@gmail.com>
2017-06-29 06:24:07 +0200

    feat: add lamp behaviour.

---
+++ Assets/Scripts/Level01/LightLampBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level01
+{
+    public class LightLampBehaviour : MonoBehaviour
+    {
+        public int _toggle = 0;
+        private float _timer = 0;
+        private bool _isCount = false;
+        private GvrAudioSource _lamp_sound;
+
+        public GameObject lamp;
+        public int solution;
+        public int mytime;
+        public bool cupboaredisopen = false;
+
+        void Start()
+        {
+            lamp.SetActive(false);
+            _lamp_sound = GetComponent<GvrAudioSource>();
+        }
+
+        void Update()
+        {
+            if (_isCount)
+            {
+                _timer += Time.deltaTime;
+            }
+
+            if (_timer >= mytime && _toggle != 0)
+            {
+                CheckSolution();
+                _isCount = false;
+            }
+        }
+
+
+        public void CheckSolution()
+        {
+            if (_toggle == solution)
+            {
+                lamp.SetActive(true);
+                foreach (Transform light in lamp.transform)
+                {
+                    light.gameObject.GetComponent<Light>().color = Color.green;
+                }
+                cupboaredisopen = true;
+            }
+
+            else
+            {
+                lamp.SetActive(true);
+                foreach (Transform light in lamp.transform)
+                {
+                    light.gameObject.GetComponent<Light>().color = Color.red;
+                }
+            }
+
+            _timer = 0;
+            _toggle = 0;
+        }
+
+        public void LightToggle()
+        {
+            foreach (Transform light in lamp.transform)
+            {
+                light.gameObject.GetComponent<Light>().color = new Color32(246, 255, 204, 255);
+            }
+            if (!lamp.activeInHierarchy)
+            {
+                lamp.SetActive(true);
+                _toggle++;
+            }
+
+            else
+            {
+                lamp.SetActive(false);
+            }
+            _timer = 0;
+        }
+
+        public void OnLampClick()
+        {
+            if (_timer < mytime || _toggle == 0)
+                LightToggle();
+
+            _isCount = true;
+
+            _lamp_sound.Play();
+        }
+    }
+}
