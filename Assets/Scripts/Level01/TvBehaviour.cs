e32a982bdcf3546c12ebb0e0fc705915327b30db
Waleed Yaser <waleedyaser95@gmail.com>
2017-09-02 19:38:27 +0200

    feat: add menu button

--- Assets/Scripts/Level01/TvBehaviour.cs
+++ Assets/Scripts/Level01/TvBehaviour.cs
@@ -10,13 +10,16 @@ namespace Level01
         public GameObject JoyStick;
         public GameObject RussiaPuzzle;
         private VideoPlayer _videoPlayer;
+        private MeshRenderer _meshRenderer;
 
         // Use this for initialization
         void Start()
         {
+            _meshRenderer = GetComponent<MeshRenderer>();
             _videoPlayer = GetComponent<VideoPlayer>();
             RussiaPuzzle.SetActive(false);
             _videoPlayer.enabled = false;
+            _meshRenderer.enabled = false;
         }
 
         public void OnTvClick()
@@ -26,12 +29,14 @@ namespace Level01
                 _videoPlayer.enabled = true;
                 JoyStick.SetActive(false);
                 RussiaPuzzle.SetActive(true);
+                _meshRenderer.enabled = true;
             }
             else
             {
                 JoyStick.SetActive(true);
                 _videoPlayer.enabled = false;
                 RussiaPuzzle.SetActive(false);
+                _meshRenderer.enabled = false;
             }
 
         }
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public void Load(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Load(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
